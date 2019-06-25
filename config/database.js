cf04fdbf57e8fd37a59d27ea81f1324973e1046b
Zech <33843491+zechheneveld@users.noreply.github.com>
2019-06-25 09:52:53 -0600

    MongoDB Atlas added

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/nodekb',
+//   database: 'mongodb://localhost:27017/nodekb',
+  database: 'mongodb+srv://dbUser:2D6lpGHOdlHlzIfm@cluster0-bb4re.mongodb.net/nodekb',
   secret: 'yoursecret'
 }
